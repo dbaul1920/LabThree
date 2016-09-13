0b94f6d8f698bb39f715faa9ab0509f2732670ab
Domonique Baul <dbaul1920@gmail.com>
2016-09-13 15:31:38 -0400

    intial setup

---
+++ src/labThree.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class labThree {
+
+	public static void main(String[] args, int intNext) {
+		// TODO Auto-generated method stub
+		System.out.println("Learn your squares and cubes!");
+		System.out.println("Please enter an integer to begin.");
+		// read user input
+		Scanner input = new Scanner(System.in);
+	
+
+		// Read page 185 for examples
+
+		//formula for calculating squares
+		
+		
+		//formula for calculating cubes
+		
+		//Print headers
+		System.out.println("Number		Squared		Cubed");
+		System.out.println("======		======		======");
+		// Print all numbers between input and 1
+		
+		//Print square of all numbers between input and 1
+		
+		//Print cube of all numbers between input and 1
+
+
+
+//Do out want to continue prompt
+//System.out.println("Continue?");
+// i = input.nextInt();
+//for(int i = 1; i <= intNext; i++){}
+
+	}
+
+}
