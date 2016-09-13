39c2de5afe6000f279fa822729be926075ee2a0c
Domonique Baul <dbaul1920@gmail.com>
2016-09-13 17:11:38 -0400

    added while loop

--- src/labThree.java
+++ src/labThree.java
@@ -5,8 +5,11 @@ public class labThree {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Learn your squares and cubes!");
+		String cont = null;
+		boolean userContinue = true;
+		while(userContinue){
 		System.out.println("Please enter an integer to begin.");
-		
+			
 		// read user input
 		Scanner input = new Scanner(System.in);
 		int x = input.nextInt();
@@ -20,18 +23,25 @@ public class labThree {
 		//formula for calculating squares
 		//formula for calculating cubes
 		for(int i = 1; i <= x; i++){System.out.println(i+ "\t\t" + facMe2(i) + "\t\t" +facMe(i));}
-//		if(x>=0){
-//			System.out.println(x + "\t\t" + facMe2(x) + "\t\t" +facMe(x));
-//		} else { 
-//		System.out.println("Nah bruh, that's not gonna cut it.");
-//				}
+	
 
-
-//Do out want to continue prompt
-//System.out.println("Continue?");
-
-
+		//Do out want to continue prompt
+		
+		//Ask if you want to continue	
+		System.out.println("Continue?");
+		Scanner input1 = new Scanner(System.in);
+		String inputContinue = input1.nextLine();
+		if(inputContinue.equalsIgnoreCase("y")){
+			userContinue = true;
+		} else {
+			userContinue = false;
+			System.out.println("Ok bye!");
+		}
+	}//close while loop
+	
+		
 	}
+	
 	
 	public static int facMe(int x){
 		 
@@ -50,9 +60,5 @@ public class labThree {
 			
 			return (x*x);
 		}
-	}
-	private static int t(int i) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 }
