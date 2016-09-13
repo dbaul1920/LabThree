136bb86e1302af4b2b4b53236aa5ec8558a410e1
Domonique Baul <dbaul1920@gmail.com>
2016-09-13 15:45:50 -0400

    added math power method

--- src/labThree.java
+++ src/labThree.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 
 public class labThree {
 
-	public static void main(String[] args, int intNext) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Learn your squares and cubes!");
 		System.out.println("Please enter an integer to begin.");
 		// read user input
 		Scanner input = new Scanner(System.in);
-	
-
+		int i = input.nextInt();
+		int intNext = 0;
+		
 		// Read page 185 for examples
-
 		//formula for calculating squares
-		
-		
 		//formula for calculating cubes
-		
+		for(i = 1; i <= intNext; i++){
+			System.out.println(Math.pow(i,2));
+			System.out.println(Math.pow(i,3));
+		}
 		//Print headers
+		boolean x = false;
+		if(x=true){
 		System.out.println("Number		Squared		Cubed");
 		System.out.println("======		======		======");
+		} else { x = false;
+			
+		}
+		
 		// Print all numbers between input and 1
 		
 		//Print square of all numbers between input and 1
@@ -30,8 +37,7 @@ public class labThree {
 
 //Do out want to continue prompt
 //System.out.println("Continue?");
-// i = input.nextInt();
-//for(int i = 1; i <= intNext; i++){}
+
 
 	}
 
